ad391d7922246dde5bde348e366259be47993623
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 15:10:45 +0530

    adding JoinBlockComponent

---
+++ src/JoinBlockComponent.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {useStyle} from './hooks'
+import withContext from './withContext'
+
+interface JBCProps {
+    w : number, 
+    h : number, 
+    scale : number, 
+    onClick : Function
+}
+const JoinBlockComponent = (props : JBCProps) => {
+    const {w, h, scale, onClick} = props
+    const {parentStyle, lineStyle, blockStyle} = useStyle(w, h, scale) 
+    return (
+        <React.Fragment>
+            <div style ={parentStyle()}>
+                {[0, 1].map(i => (<div style = {blockStyle(i)} key = {`block_${i}`}></div>))}
+                <div style = {lineStyle()}></div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default withContext(JoinBlockComponent)
